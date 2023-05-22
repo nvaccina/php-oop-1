cff09fba4cd5f78f3a981721bba704369e5db6ce
nvaccina <niccolovac@gmail.com>
2023-05-22 14:27:51 +0200

    Creata class Movie, variabili d'istanza, construct

--- models/Movie.php
+++ models/Movie.php
@@ -1,2 +1,18 @@
 <?php
 
+class Movie{
+  public $titolo;
+  public $regista;
+  public $durata;
+
+  function __construct($_titolo, $_regista, $_durata){
+    $this->titolo = $_titolo;
+    $this->regista = $_regista;
+    $this->durata = $_durata;
+  }
+}
+
+$FastAndFurios = new Movie('Fast And Furios ', 'Rob Cohen', 106);
+
+
+var_dump($FastAndFurios);
